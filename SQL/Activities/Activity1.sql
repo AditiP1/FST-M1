c34220143780fe8c2f98dc5bf1c4db9fbeb9f2c1
AditiP1 <142587338+AditiP1@users.noreply.github.com>
2023-10-02 14:51:06 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: Activity1
+REM   Solution for Activity1
+
+CREATE TABLE salesman ( 
+    salesman_id int primary key, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
